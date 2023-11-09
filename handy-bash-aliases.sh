2be161186e49e2d6fc881e159404bf826ee375d4
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-11-09 10:27:54 +0100

    add handy-bash-aliases

---
+++ handy-bash-aliases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Open the Xcode workspace from the command line
+alias xcopen='open -a Xcode *.xcworkspace'
+
+alias vc='code .'
+
+alias f72="~/git-repos/developer-productivity/format-max-line-length.sh 72"
