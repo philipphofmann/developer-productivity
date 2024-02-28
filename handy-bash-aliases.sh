04c2c25edcee5c7d59bdbec2edffe4ddd7d72800
Philipp Hofmann <philipp.hofmann@sentry.io>
2024-02-28 09:29:21 +0100

    add mypy-edited

--- handy-bash-aliases.sh
+++ handy-bash-aliases.sh
@@ -8,3 +8,5 @@ alias vc='code .'
 alias f72="~/git-repos/developer-productivity/format-max-line-length.sh 72"
 
 alias next-pr-number='~/git-repos/developer-productivity/get-next-pr-number.sh'
+
+alias mypy-edited='git diff --name-only --cached | grep '\.py' | xargs -r mypy'
