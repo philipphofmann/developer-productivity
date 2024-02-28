c0616c35c30cb083b0a2e01ff61f97533f3ad5a9
Philipp Hofmann <philipp.hofmann@sentry.io>
2024-02-28 09:30:15 +0100

    open bash profiles in VSCode

--- handy-bash-aliases.sh
+++ handy-bash-aliases.sh
@@ -10,3 +10,5 @@ alias f72="~/git-repos/developer-productivity/format-max-line-length.sh 72"
 alias next-pr-number='~/git-repos/developer-productivity/get-next-pr-number.sh'
 
 alias mypy-edited='git diff --name-only --cached | grep '\.py' | xargs -r mypy'
+
+alias vc-bash-profiles="code  ~/.profile ~/.bash_profile ~/.bashrc ~/.zshrc ~/.zprofile"
